eebe709b7b629167861165bcb052de367f61054e
zhuan <zhuan982@outlook.com>
2016-11-26 00:24:15 +0800

    完成测试插件。

--- CoolQNetPlugin/TestPlugin.vb
+++ CoolQNetPlugin/TestPlugin.vb
@@ -53,6 +53,7 @@ Public Class TestPlugin
     ''' 此函数会在酷Q退出时被调用。
     ''' </summary>
     ''' <returns><see cref="Integer"/> </returns>
+    <DllExport("_eventExit")>
     Public Shared Function CoolQExited() As Integer
 
         Return 0 '固定返回0
@@ -65,7 +66,7 @@ Public Class TestPlugin
     ''' <param name="fromQQ">发送此消息的QQ号码。</param>
     ''' <param name="msg">消息的内容。</param>
     ''' <param name="font">消息所使用的字体。</param>
-    ''' <returns><see cref="Integer"/> 是否拦截消息的值，1为拦截消息。</returns>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
     <DllExport("_eventPrivateMsg")>
     Public Shared Function ProcessPrivateMessage(subType As Integer, sendTime As Integer, fromQQ As String, msg As String, font As Integer) As Integer
         '0为忽略 1为拦截
@@ -73,21 +74,136 @@ Public Class TestPlugin
         Return 0
     End Function
     ''' <summary>
-    ''' 
+    ''' 处理群聊消息。
     ''' </summary>
-    ''' <param name="subType"></param>
-    ''' <param name="sendTime"></param>
-    ''' <param name="fromGroup"></param>
-    ''' <param name="fromQQ"></param>
-    ''' <param name="fromAnonymous"></param>
-    ''' <param name="msg"></param>
-    ''' <param name="font"></param>
-    ''' <returns></returns>
+    ''' <param name="subType">消息类型，目前固定为1。</param>
+    ''' <param name="sendTime">消息发送时间的时间戳。</param>
+    ''' <param name="fromGroup">消息来源群号。</param>
+    ''' <param name="fromQQ">发送此消息的QQ号码。</param>
+    ''' <param name="fromAnonymous">发送此消息的匿名用户。</param>
+    ''' <param name="msg">消息内容。</param>
+    ''' <param name="font">消息所使用字体。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
     <DllExport("_eventGroupMsg")>
-    Public Shared Function ProcessGroupMessage(subType As Integer, sendTime As Integer, fromGroup As Integer, fromQQ As Integer, fromAnonymous As Integer,
+    Public Shared Function ProcessGroupMessage(subType As Integer, sendTime As Integer, fromGroup As Long, fromQQ As Long, fromAnonymous As String,
                                                msg As String, font As Integer) As Integer
         '0为忽略 1为拦截
 
         Return 0
     End Function
+    ''' <summary>
+    ''' 处理讨论组消息。
+    ''' </summary>
+    ''' <param name="subType">消息类型，目前固定为1。</param>
+    ''' <param name="sendTime">消息发送时间的时间戳。</param>
+    ''' <param name="fromDiscuss">消息来源讨论组号。</param>
+    ''' <param name="fromQQ">发送此消息的QQ号码。</param>
+    ''' <param name="msg">消息内容。</param>
+    ''' <param name="font">消息所使用字体。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventDiscussMsg")>
+    Public Shared Function ProcessDiscussGroupMessage(subType As Integer, sendTime As Integer, fromDiscuss As Long, fromQQ As Long, msg As String, font As Integer) As Integer
+        '0为忽略 1为拦截
+
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理群文件上传事件。
+    ''' </summary>
+    ''' <param name="subType">消息类型，目前固定为1。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromGroup">事件来源群号。</param>
+    ''' <param name="fromQQ">上传此文件的QQ号码。</param>
+    ''' <param name="file">上传的文件的信息。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventGroupUpload")>
+    Public Shared Function ProcessGroupUpload(subType As Integer, sendTime As Integer, fromGroup As Long, fromQQ As Long, file As String) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理群管理员变动事件。
+    ''' </summary>
+    ''' <param name="subType">事件类型。1为被取消管理员，2为被设置管理员。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromGroup">事件来源群号。</param>
+    ''' <param name="target">被操作的QQ。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventSystem_GroupAdmin")>
+    Public Shared Function ProcessGroupAdminChange(subType As Integer, sendTime As Integer, fromGroup As Long, target As Long) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理群成员数量减少事件。
+    ''' </summary>
+    ''' <param name="subType">事件类型。1为群员离开；2为群员被踢为；3为自己(即登录号)被踢。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromGroup">事件来源群号。</param>
+    ''' <param name="fromQQ">事件来源QQ。</param>
+    ''' <param name="target">被操作的QQ。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventSystem_GroupMemberDecrease")>
+    Public Shared Function ProcessGroupMemberDecrease(subType As Integer, sendTime As Integer, fromGroup As Long, fromQQ As Long, target As Long) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理群成员添加事件。
+    ''' </summary>
+    ''' <param name="subType">事件类型。1为管理员已同意；2为管理员邀请。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromGroup">事件来源群号。</param>
+    ''' <param name="fromQQ">事件来源QQ。</param>
+    ''' <param name="target">被操作的QQ。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventSystem_GroupMemberIncrease")>
+    Public Shared Function ProcessGroupMemberIncrease(subType As Integer, sendTime As Integer, fromGroup As Long, fromQQ As Long, target As Long) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理好友已添加事件。
+    ''' </summary>
+    ''' <param name="subType">事件类型。固定为1。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromQQ">事件来源QQ。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventFriend_Add")>
+    Public Shared Function ProcessFriendsAdded(subType As Integer, sendTime As Integer, fromQQ As Long) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理好友添加请求。
+    ''' </summary>
+    ''' <param name="subType">事件类型。固定为1。</param>
+    ''' <param name="sendTime">事件发生时间的时间戳。</param>
+    ''' <param name="fromQQ">事件来源QQ。</param>
+    ''' <param name="msg">附言内容。</param>
+    ''' <param name="font">消息所使用字体。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    <DllExport("_eventRequest_AddFriend")>
+    Public Shared Function ProcessAddFriendRequest(subType As Integer, sendTime As Integer, fromQQ As Long, msg As String, font As Integer) As Integer
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 处理加群请求。
+    ''' </summary>
+    ''' <param name="subType">请求类型。1为他人申请入群；2为自己(即登录号)受邀入群。</param>
+    ''' <param name="sendTime">请求发送时间戳。</param>
+    ''' <param name="fromGroup">要加入的群的群号。</param>
+    ''' <param name="fromQQ">发送此请求的QQ号码。</param>
+    ''' <param name="msg">附言内容。</param>
+    ''' <param name="responseFlag">用于处理请求的标识。</param>
+    ''' <returns><see cref="Integer"/> 是否拦截消息的值，0为忽略消息，1为拦截消息。</returns>
+    Public Shared Function ProcessJoinGroupRequest(subType As Integer, sendTime As Integer, fromGroup As Long, fromQQ As Long, msg As String, responseFlag As String) As Integer
+        Return 0
+    End Function
+    '菜单示例
+    '假设菜单中在json文件中的设置如下
+    '{
+    '        "name""设置A",			//菜单名称
+    '       "function":"_menuA"		//菜单对应函数
+    '}
+    '则
+    '<DllExport("菜单对应函数")>
+    'Public Shared Function <执行过程名称，可与菜单对应函数不同>() As Integer
+    '    Return 0 '固定返回0
+    'End Function
 End Class
