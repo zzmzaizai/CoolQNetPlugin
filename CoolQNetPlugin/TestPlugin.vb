946ead6d761a18a9f21a22d4dab10a84de317ae8
zhuan <zhuan982@outlook.com>
2016-11-25 22:39:47 +0800

    向测试插件添加构造函数以及Initalize过程。

--- CoolQNetPlugin/TestPlugin.vb
+++ CoolQNetPlugin/TestPlugin.vb
@@ -1,12 +1,19 @@
 ﻿''' <summary>
 ''' 酷Q.NET测试插件。
 ''' </summary>
+''' 类名可改变
 Public Class TestPlugin
 
     Private Const ApiVersion As Integer = 9 'Api版本号，若酷Q官方SDK没有更新此版本号，请勿改动此值
     'AppID 
     Private Const AppId As String = "com.net.hotplug.test"
 
+    ''' <summary>
+    ''' 初始化<see cref="TestPlugin"/>的新实例。 
+    ''' </summary>
+    Public Sub New()
+        '在这里写插件的加载代码。
+    End Sub
     ''' <summary>
     ''' 插件被开启时发生。
     ''' </summary>
@@ -35,5 +42,14 @@ Public Class TestPlugin
         '请勿修改此函数
         Return (ApiVersion.ToString + "," + AppId)
     End Function
+    ''' <summary>
+    ''' 向酷Q提供此插件的AuthCode。
+    ''' </summary>
+    ''' <param name="authcode">由酷Q提供的AuthCode。</param>
+    ''' <returns><see cref="Integer"/> </returns>
+    <DllExport("Initialize")>
+    Public Shared Function Initialize(authcode As Integer) As Integer
+        '请勿更改此函数
 
+    End Function
 End Class
