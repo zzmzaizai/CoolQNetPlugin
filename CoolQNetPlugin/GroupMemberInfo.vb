0d33b1b6f6186d1a428dfe4b920622b2768a27ac
zhuan <zhuan982@outlook.com>
2016-11-27 14:10:26 +0800

    完成群成员基础信息类。

--- CoolQNetPlugin/GroupMemberInfo.vb
+++ CoolQNetPlugin/GroupMemberInfo.vb
@@ -28,4 +28,139 @@ Public NotInheritable Class GroupMemberInfo
         info_level = level
 
     End Sub
+    ''' <summary>
+    ''' 获取此群成员在其个人资料上所填写的年龄。
+    ''' </summary>
+    ''' <returns><see cref="Integer"/> </returns>
+    Public ReadOnly Property Age As Integer
+        Get
+            Return info_age
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员在其个人资料上所填写的区域。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property Area As String
+        Get
+            Return info_area
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的身份。
+    ''' </summary>
+    ''' <returns><see cref="String"/></returns>
+    Public ReadOnly Property Authority As String
+        Get
+            Return info_authority
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取指示此群成员是否能够修改所有群成员名片的值。
+    ''' </summary>
+    ''' <returns><see cref="Boolean"/> </returns>
+    Public ReadOnly Property CanModifyInGroupName As Boolean
+        Get
+            Return p_canmodifycard
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员在其个人资料上所填写的性别。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property Gender As String
+        Get
+            Return info_gender
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的群名片。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property InGroupName As String
+        Get
+            Return info_card
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的头衔。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property Title As String
+        Get
+            Return info_title
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员所在群号。
+    ''' </summary>
+    ''' <returns><see cref="Long"/> </returns>
+    Public ReadOnly Property GroupId As Long
+        Get
+            Return info_groupnumber
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取指示此群成员是否有不良记录的值。
+    ''' </summary>
+    ''' <returns><see cref="Boolean"/> </returns>
+    Public ReadOnly Property HasBadRecord As Boolean
+        Get
+            Return info_hasbadrecord
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取头衔过期时间。
+    ''' </summary>
+    ''' <returns><see cref="Integer"/> </returns>
+    Public ReadOnly Property TitleExpirationTime As Integer
+        Get
+            Return info_titleexpirationtime
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的入群时间。
+    ''' </summary>
+    ''' <returns><see cref="Date"/> </returns>
+    Public ReadOnly Property JoinTime As Date
+        Get
+            Return info_joingroupdate
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员最后发言日期。
+    ''' </summary>
+    ''' <returns><see cref="Date"/> </returns>
+    Public ReadOnly Property LastSpeakingTime As Date
+        Get
+            Return info_lastspeaktime
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的群内等级。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property Level As String
+        Get
+            Return info_level
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的昵称。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public ReadOnly Property NickName As String
+        Get
+            Return info_nickname
+        End Get
+    End Property
+    ''' <summary>
+    ''' 获取此群成员的QQ号码。
+    ''' </summary>
+    ''' <returns><see cref="Long"/> </returns>
+    Public ReadOnly Property Number As Long
+        Get
+            Return info_qqnumber
+        End Get
+    End Property
 End Class
