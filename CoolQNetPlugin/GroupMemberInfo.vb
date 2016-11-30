efd6e7e72db895dcdc84c442b04b323083dbbb40
zhuan <zhuan982@outlook.com>
2016-11-30 22:17:45 +0800

    数据恢复。

--- CoolQNetPlugin/GroupMemberInfo.vb
+++ CoolQNetPlugin/GroupMemberInfo.vb
@@ -3,23 +3,79 @@
 ''' </summary>
 Public NotInheritable Class GroupMemberInfo
 
-    End Sub
-    Friend Sub New(age As Integer, area As String, authority As String, canmodifycard As Boolean,
-                   gender As String, card As String, title As String, groupnumber As Long, hasbardrecord As Boolean,
-                   titleexpirationtime As Integer, joingroupdate As Date, lastspeaktime As Date, level As String,
-                   nickname As String, qqnumber As Long)
-        info_age = age
-        info_area = area
-        info_authority = authority
-        p_canmodifycard = canmodifycard
-        info_gender = gender
-        info_card = card
-        info_title = title
-        info_groupnumber = groupnumber
-        info_hasbadrecord = hasbardrecord
-        info_titleexpirationtime = titleexpirationtime
-        info_joingroupdate = joingroupdate
-        info_level = level
-
-    End Sub
+    ''' <summary>
+    ''' 此群成员在其个人资料上所填写的年龄。
+    ''' </summary>
+    ''' <returns><see cref="Integer"/> </returns>
+    Public Property Age As Integer
+    ''' <summary>
+    ''' 此群成员在其个人资料上所填写的区域。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property Area As String
+    ''' <summary>
+    ''' 此群成员的身份。
+    ''' </summary>
+    ''' <returns><see cref="String"/></returns>
+    Public Property Authority As String
+    ''' <summary>
+    ''' 指示此群成员是否能够修改所有群成员名片的值。
+    ''' </summary>
+    ''' <returns><see cref="Boolean"/> </returns>
+    Public Property CanModifyInGroupName As Boolean
+    ''' <summary>
+    ''' 此群成员在其个人资料上所填写的性别。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property Gender As String
+    ''' <summary>
+    ''' 此群成员的群名片。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property InGroupName As String
+    ''' <summary>
+    ''' 此群成员的头衔。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property Title As String
+    ''' <summary>
+    ''' 此群成员所在群号。
+    ''' </summary>
+    ''' <returns><see cref="Long"/> </returns>
+    Public Property GroupId As Long
+    ''' <summary>
+    ''' 指示此群成员是否有不良记录的值。
+    ''' </summary>
+    ''' <returns><see cref="Boolean"/> </returns>
+    Public Property HasBadRecord As Boolean
+    ''' <summary>
+    ''' 头衔过期时间。
+    ''' </summary>
+    ''' <returns><see cref="Integer"/> </returns>
+    Public Property TitleExpirationTime As Integer
+    ''' <summary>
+    ''' 此群成员的入群时间。
+    ''' </summary>
+    ''' <returns><see cref="Date"/> </returns>
+    Public Property JoinTime As Date
+    ''' <summary>
+    ''' 此群成员最后发言日期。
+    ''' </summary>
+    ''' <returns><see cref="Date"/> </returns>
+    Public Property LastSpeakingTime As Date
+    ''' <summary>
+    ''' 此群成员的群内等级。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property Level As String
+    ''' <summary>
+    ''' 此群成员的昵称。
+    ''' </summary>
+    ''' <returns><see cref="String"/> </returns>
+    Public Property NickName As String
+    ''' <summary>
+    ''' 此群成员的QQ号码。
+    ''' </summary>
+    ''' <returns><see cref="Long"/> </returns>
+    Public Property Number As Long
 End Class
--- CoolQNetPlugin/PluginHelper.vb
+++ CoolQNetPlugin/PluginHelper.vb
@@ -3,54 +3,6 @@
 ''' </summary>
 Module PluginHelper
     ''' <summary>
-    ''' 表示忽略消息的值。
-    ''' </summary>
-    Public Const Event_Ignore As Integer = 0
-    ''' <summary>
-    ''' 表示拦截消息的值。
-    ''' </summary>
-    Public Const Event_Block As Integer = 1
-    ''' <summary>
-    ''' 表示请求类型为加群请求。
-    ''' </summary>
-    Public Const RequestType_GroupJoin As Integer = 1
-    ''' <summary>
-    ''' 表示请求类型为邀请加群请求。
-    ''' </summary>
-    Public Const RequestType_GroupInvtiation As Integer = 2
-    ''' <summary>
-    ''' 表示酷Q日志等级为调试。
-    ''' </summary>
-    Public Const Log_Debug As Integer = 0
-    ''' <summary>
-    ''' 表示酷Q日志等级为信息。
-    ''' </summary>
-    Public Const Log_Info As Integer = 10
-    ''' <summary>
-    ''' 表示酷Q日志等级为信息（成功）。
-    ''' </summary>
-    Public Const Log_InfoSuccess As Integer = 11
-    ''' <summary>
-    ''' 表示酷Q日志等级为信息（接收）。
-    ''' </summary>
-    Public Const Log_InfoReceive As Integer = 12
-    ''' <summary>
-    ''' 表示酷Q日志等级为信息（发送）。
-    ''' </summary>
-    Public Const Log_InfoSend As Integer = 13
-    ''' <summary>
-    ''' 表示酷Q日志等级为警告。
-    ''' </summary>
-    Public Const Log_Warning As Integer = 20
-    ''' <summary>
-    ''' 表示酷Q日志等级为错误。
-    ''' </summary>
-    Public Const Log_Error As Integer = 30
-    ''' <summary>
-    ''' 表示酷Q日志等级为致命错误。
-    ''' </summary>
-    Public Const Log_Fatal As Integer = 40
-    ''' <summary>
     ''' 酷Q Api实例。
     ''' </summary>
     Friend CQ As New CoolQApi
